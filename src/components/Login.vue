7726d9b585d8ad7ad97c23e1e20ca0a09628b83b
Pablo N Manzur <pablonicolasmanzur@gmail.com>
2024-10-06 23:02:13 -0300

    Login Component Creado

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<script>
+    import {ref} from 'vue'
+
+    const user = ref('')
+    const password = ref('')
+
+</script>
+
+
+<template>
+    <div>
+
+        <input type="text" placeholder="Usuario" v-model="user">
+        <input type="text" placeholder="Contraseña" v-model="password">
+
+        <button type="submit">Login</button>
+
+    </div>
+
+</template>
+
+<style scoped>
+</style>
--- src/components/prueba1.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-</script>
-
-
-<template>
-
-   <h1> probando</h1> 
-</template>
